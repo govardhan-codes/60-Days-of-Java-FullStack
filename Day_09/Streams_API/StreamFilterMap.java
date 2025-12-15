525f5d302f49daeb1ef14df51c034b23740a1c96
Govardhan A <govardhan0810@gmail.com>
2025-12-15 20:40:19 +0530

    Create StreamFilterMap.java

---
+++ Day_09/Streams_API/StreamFilterMap.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class StreamFilterMap {
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(1,2,3,4,5,6);
+
+        list.stream()
+            .filter(n -> n % 2 == 0)
+            .map(n -> n * n)
+            .forEach(System.out::println);
+    }
+}
